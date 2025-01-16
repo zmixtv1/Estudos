1b1f73388f0200a8223eefb69a497cac499f87bb
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-16 09:42:15 -0300

    Exercicio completo

---
+++ C#/ExercicioProduto/ExercicioProduto/Estoque.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExercicioProduto
+{
+    internal class Estoque
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+        
+
+        public double ValorTotalEstoque() 
+        {
+            double total = Preco * Quantidade;
+            return total;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, ${Preco.ToString("F2", CultureInfo.InvariantCulture)} e {Quantidade} unidades.\nTotal: ${ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ C#/ExercicioProduto/ExercicioProduto/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ExercicioProduto;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Estoque p = new Estoque();
+
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Produto: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nDados do Produto: {p}");
+            Console.Write("Deseja adicionar ou remover produto? [Sim/nao]: ");
+            string resposta = Console.ReadLine().ToLower();
+
+            while (resposta == "sim")
+            {
+                Console.Write("\nPara Adicionar Digite [1] e Para Remover digite [2]: ");
+                int resp = int.Parse(Console.ReadLine());
+                if (resp == 1)
+                {
+                    Console.Write("\nDigite a quantidade que deseja adicionar: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.AdicionarProdutos(qte);
+                    Console.WriteLine($"\nDados do Produto: {p}");
+                }
+                else if (resp == 2)
+                {
+                    Console.Write("\nDigite a quantidade que deseja remover: ");
+                    int qte = int.Parse(Console.ReadLine());
+                    p.RemoverProdutos(qte);
+                    Console.WriteLine($"\nDados do Produto: {p}");
+                }
+                else 
+                {
+                    Console.WriteLine("Digito não encontrado, tente novamente...\n");
+                }
+                Console.Write("Deseja adicionar ou remover produto? [Sim/nao]: ");
+                resposta = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
