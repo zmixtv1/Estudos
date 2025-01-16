d3731d19d39efd95d37d89c304b1a6e75036751e
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-16 11:15:40 -0300

    Exercicios propostos ok

---
+++ C#/ExercicioTriangulo/ExercicioTriangulo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ExercicioTriangulo;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+
+            Console.WriteLine("Entre com a largura e a altura do triangulo: ");
+            Console.Write("Largura: ");
+            t.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("altura: ");
+            t.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(t);
+        }
+    }
+}
---
+++ C#/ExercicioTriangulo/ExercicioTriangulo/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioTriangulo
+{
+    internal class Triangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+        public double Diagonal()
+        {
+            return Math.Sqrt((Largura*Largura) + (Altura*Altura));
+        }
+
+        public override string ToString()
+        {
+            return $"Area = {Area().ToString("F2",CultureInfo.InvariantCulture)}\nPerímetro = {Perimetro().ToString("F2", CultureInfo.InvariantCulture)}\nDiagonal = {Diagonal().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
