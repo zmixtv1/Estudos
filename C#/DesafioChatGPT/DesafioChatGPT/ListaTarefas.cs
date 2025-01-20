8c9d716b476938143789c1e47d1b6358ed12f0aa
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-20 16:49:33 -0300

    Add desafio pessoal

---
+++ C#/DesafioChatGPT/DesafioChatGPT/ListaTarefas.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioChatGPT
+{
+    internal class ListaTarefas
+    {
+        public int IdTarefa { get; private set; }
+        public string Descricao { get; private set; }
+        public string Status { get; private set; }
+
+
+        public void AdicionarTarefa(string descricao, string status)
+        {
+
+        }
+
+    }
+}
+
+
+/*
+ === Sistema de Gerenciamento de Tarefas ===  
+1. Adicionar tarefa  
+2. Listar tarefas  
+3. Concluir tarefa  
+4. Remover tarefa  
+5. Sair  
+
+Escolha uma opção: 1  
+Digite a descrição da tarefa: Estudar C#  
+Tarefa adicionada com sucesso!  
+
+Escolha uma opção: 2  
+ID: 1 | Descrição: Estudar C# | Status: Pendente  
+
+Escolha uma opção: 3  
+Digite o ID da tarefa concluída: 1  
+Tarefa marcada como concluída!  
+
+Escolha uma opção: 2  
+ID: 1 | Descrição: Estudar C# | Status: Concluída  
+*/
---
+++ C#/DesafioChatGPT/DesafioChatGPT/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+/*
+ === Sistema de Gerenciamento de Tarefas ===  
+1. Adicionar tarefa  
+2. Listar tarefas  
+3. Concluir tarefa  
+4. Remover tarefa  
+5. Sair  
+
+Escolha uma opção: 1  
+Digite a descrição da tarefa: Estudar C#  
+Tarefa adicionada com sucesso!  
+
+Escolha uma opção: 2  
+ID: 1 | Descrição: Estudar C# | Status: Pendente  
+
+Escolha uma opção: 3  
+Digite o ID da tarefa concluída: 1  
+Tarefa marcada como concluída!  
+
+Escolha uma opção: 2  
+ID: 1 | Descrição: Estudar C# | Status: Concluída  
+*/
