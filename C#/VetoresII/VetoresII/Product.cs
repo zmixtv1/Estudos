de4d9c67789206ee373417b44c90e66eb8501d51
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-27 09:49:28 -0300

    Vetores II

---
+++ C#/VetoresII/VetoresII/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VetoresII
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
