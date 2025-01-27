b8c2961237eac19d3f2de9d34b412670e00672ba
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-27 11:01:01 -0300

    Practical exercise

---
+++ C#/ExercicioVetores/ExercicioVetores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ExercicioVetores;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Quartos[] vect = new Quartos[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room: ");
+                int room = int.Parse(Console.ReadLine());
+                if (vect[room] != null)
+                {
+                    Console.WriteLine("Room already occupied!");
+
+                    Console.Write("Enter another Room: ");
+                    int otherRoom = int.Parse(Console.ReadLine());
+                    vect[otherRoom] = new Quartos(name, email);
+                }
+                else
+                {
+                    vect[room] = new Quartos(name, email);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine($"{i}: {vect[i]}");
+                }
+            }
+        }
+    }
+}
---
+++ C#/ExercicioVetores/ExercicioVetores/Quartos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioVetores
+{
+    internal class Quartos
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Quartos(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+
+        public override string? ToString()
+        {
+            return Name + "," + Email;
+        }
+    }
+}
