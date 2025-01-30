a2971bcb6baf6ba79072065047d7d14c75847160
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-01-30 16:50:24 -0300

    Fim

---
+++ C#/ExercicioFixacaoListas/ExercicioFixacaoListas/Funcionarios.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacaoListas
+{
+    internal class Funcionarios
+    {
+        public class InfoFuncionarios
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public double Salary { get; set; }
+        }
+
+        public List<InfoFuncionarios> funcionarios { get; set; } = new List<InfoFuncionarios>();
+
+        public void AdicionarFuncionario(int id, string name, double salary)
+        {
+            funcionarios.Add(new InfoFuncionarios { Id = id, Name = name, Salary = salary });
+        }
+
+        public string getFuncionarios()
+        {
+            string resultado = null;
+            foreach (var funcionario in funcionarios)
+            {
+                resultado += $"{funcionario.Id}, {funcionario.Name}, {funcionario.Salary}\n";
+            }
+            return resultado;
+        }
+
+        public void setSalaryFuncionario(int id, double percentage)
+        {
+            foreach (var funcionario in funcionarios)
+            {
+                if (funcionario.Id == id)
+                {
+                    funcionario.Salary += funcionario.Salary * (percentage / 100);
+                }
+            }
+        }
+
+    }
+}
---
+++ C#/ExercicioFixacaoListas/ExercicioFixacaoListas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using ExercicioFixacaoListas;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios listaFuncionario = new Funcionarios();
+
+            Console.Write("Quantos funcionarios deseja cadastrar: ");
+            int cadastro = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cadastro; i++)
+            {
+                Console.WriteLine($"\nEmpregado : #{i + 1}");
+
+                Console.Write($"Id: ");
+                int Id = int.Parse(Console.ReadLine());
+                Console.Write($"Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write($"Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                listaFuncionario.AdicionarFuncionario(Id, nome, salario);
+            }
+
+            Console.Write("\nEntre com o ID que deseja almentar o salario: ");
+            int IdAlmento = int.Parse(Console.ReadLine());
+            Console.Write("\nEntre com a porcentagem: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            listaFuncionario.setSalaryFuncionario(IdAlmento, porcentagem);
+
+            Console.WriteLine("\nLista de funcionarios Atulaizada:");
+            Console.WriteLine(listaFuncionario.getFuncionarios());
+
+
+        }
+    }
+}
