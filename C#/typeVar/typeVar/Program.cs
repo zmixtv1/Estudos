985d0885ff0f55cd67b2f87c77a88146485b9073
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-02-01 20:19:51 -0300

    Condional in line

---
+++ C#/typeVar/typeVar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double desconto = (preco < 20.0) ? desconto = (preco * 0.1) : desconto = (preco * 0.05);
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
