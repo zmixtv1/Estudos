b264faade66d3e6e86eba45c85fab64850f4c036
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-02-01 14:17:04 -0300

    learning about matriz

---
+++ C#/Matriz/Matriz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the order value: ");
+            int order = int.Parse(Console.ReadLine());
+            int negative = 0;
+
+            int[,] mat = new int[order, order];
+
+            for (int i = 0; i < order; i++)
+            {
+
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < order; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+
+                    if (mat[i, j] < 0)
+                    {
+                        negative++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Main diagonal: ");
+
+            for (int i = 0; i < order; i++)
+            {
+                Console.Write($"{mat[i, i]} ");
+            }
+
+            Console.Write($"\nNegative numbers = {negative}");
+        }
+    }
+}
