3f9e3de1f8ad65a6bab1df500b0824cd272c64ab
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-04-24 12:26:02 -0300

    .

--- C#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ C#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5b3361326c5c40e898b1af7c488af6fac155b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985d0885ff0f55cd67b2f87c77a88146485b9073")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ python/secao3/video72.py
@@ -0,0 +1,5 @@
+texto = 'python'
+
+print("texto, posição")
+for c in texto:
+    print(texto[c], c)
