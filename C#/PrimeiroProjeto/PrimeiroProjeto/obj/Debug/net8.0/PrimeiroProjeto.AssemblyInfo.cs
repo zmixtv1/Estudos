e1cfa1687bb2a8e7b0699a6b1c0314205110f0f7
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-06-24 22:47:28 -0300

    Update assembly info and cache for build
    Regenerated AssemblyInfo and cache files as part of the build process. No source code changes included.

--- C#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
+++ C#/PrimeiroProjeto/PrimeiroProjeto/obj/Debug/net8.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f67ee578aa92c3d97d534e62ab56c73261826b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee4866e836eec05bb5e6de436893c1d1af859193")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
