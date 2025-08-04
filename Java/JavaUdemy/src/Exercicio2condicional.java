2d40042d33e9100ccf6a758f87b0b00d2ee773e1
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-08-04 14:17:09 -0300

    Test WHILE

--- Java/JavaUdemy/src/Exercicio2condicional.java
+++ Java/JavaUdemy/src/Exercicio2condicional.java
@@ -2,18 +2,32 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicio2condicional {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Digite o número: ");
-        int num = scanner.nextInt();
+        while (true) {
+            System.out.printf("Digite o número: ");
+            int num = scanner.nextInt();
 
-        if (num % 2 == 0){
-            System.out.println("Par");
-        }else{
-            System.out.println("Impar");
+            if (num % 2 == 0) {
+                System.out.println("Par");
+            } else {
+                System.out.println("Impar");
+            }
+
+            System.out.printf("Deseja Continuar[S/N]: ");
+            String letra = scanner.next();
+            letra = letra.toUpperCase(Locale.ROOT);
+
+            if (letra.equals("N")){
+                break;
+            }else{
+                continue;
+            }
+
+
         }
-
     }
 }
+
