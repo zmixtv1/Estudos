04472ebce7da59a9ea9f57566764007920910616
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2025-11-11 18:22:44 -0300

    Basico
    Estuda todo o codigo pois está  somente o básico para  aprender sobre django completo

--- pythonDJANGO/tarefas/urls.py
+++ pythonDJANGO/tarefas/urls.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from . import views
 
+app_name = "tarefas"
+
 urlpatterns = [
-    path('', views.tarefas_home)
+    path('', views.tarefas_home),
+    path('adicionar/', views.tarefas_adicionar, name='adicionar')
 ]
