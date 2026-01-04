e27d127a2e3b5269566da63aaee7a96d82f7ed97
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2026-01-04 17:33:36 -0300

    .

--- Java/Contrutores/src/Main.java
+++ Java/Contrutores/src/Main.java
@@ -1,15 +1,19 @@
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        ArrayList<String> filmes = new ArrayList<>();
+
         for(int cont = 0; cont < 3 ; cont++){
 
             System.out.println("Digite o nome de um filme: ");
             String nome = sc.nextLine();
+            filmes.add(nome);
             System.out.println("Digite o ano de lançamento: ");
             int lancamento = sc.nextInt();
             System.out.println("Digite a nota do filme:");
@@ -24,6 +28,9 @@ public class Main {
             } else {
                 System.out.println("Filme ok!");
             }
+        }
+        for (String f : filmes){
+            System.out.println("Filmes cadastrado: " + f);
         }
     }
 }
