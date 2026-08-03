61d3980d9a427882a7e7387518051693b48032b8
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2026-08-03 16:49:40 -0300

    ?

--- C#/ExercicioProduto/ExercicioProduto/obj/Debug/net8.0/ExercicioProduto.AssemblyInfo.cs
+++ C#/ExercicioProduto/ExercicioProduto/obj/Debug/net8.0/ExercicioProduto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExercicioProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a323ef02bfaa040f034d219f1ae20237b74a1ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eac7316af7562530971e602c2fc3f3fc5305978")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExercicioProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExercicioProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
